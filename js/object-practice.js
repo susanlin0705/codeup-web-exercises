f2b3e4fab9e4153d183276c9482333d5e514413a
susanlin0705 <susan@Meng-hans-MBP.satx.rr.com>
2019-07-10 19:08:37 -0500

    ajax practice

--- js/object-practice.js
+++ js/object-practice.js
@@ -3,7 +3,7 @@ var makeCar = function(name1,name2){
         make:name1,
         model:name2
     }
-}
+};
 makeCar("VW", "beetle");
 
 var salesReport = {
@@ -18,43 +18,91 @@ var salesReport = {
             salesUnits:'3'
         },
         {
-            number:"2",
+            number:"3",
             firstName:'Tricia',
             lastName:'Tricason',
             salesUnits:'5'
         },
         {
-            number:"3",
+            number:"4",
             firstName:'Jeannette',
             lastName:'Jeanson',
             salesUnits:'4'
-        },],
-    getEmployeeCount: function () {
-        return this.employees.length
-    },
-    getTotalNumberOfSales:function(){
-        var total = 0
+        },
+        {
+            number:"5",
+            firstName:'Charles Emmerson',
+            lastName:'Winchester',
+            salesUnits:'2'
+        },
+        {
+            number:"6",
+            firstName:'Chet',
+            lastName:'Chedderson',
+            salesUnits:'4'
+        },
+        {
+            number:"7",
+            firstName:'Chaiam',
+            lastName:'Chaiamson',
+            salesUnits:'12'
+        },
+        {
+            number:"8",
+            firstName:'Dale',
+            lastName:'Dalesinger',
+            salesUnits:'1'
+        },
+        {
+            number:"9",
+            firstName:'Zig',
+            lastName:'Ziglar',
+            salesUnits:'50'
+        },
+        {
+            number:"10",
+            firstName:'Henry',
+            lastName:'Kisssinger',
+            salesUnits:'1'
+        },
+        {
+            number:"11",
+            firstName:'Arthur Herbert',
+            lastName:'Fonzarelli',
+            salesUnits:'23'
+        },
+        {
+            number:"12",
+            firstName:'Betty ',
+            lastName:'Boop',
+            salesUnits:'67'
+        }],
+        getEmployeeCount: function () {
+                return this.employees.length
+            },
+        getTotalNumberOfSales: function () {
+                var total = 0;
 
-        this.employees.forEach(function (employee) {
-            total += Number(employee.salesUnits)
-        });
-        return total
-    },
-    getAverageSalesPerEmployee:function(){
+                this.employees.forEach(function (employee) {
+                    total += Number(employee.salesUnits)
+                });
+                return total
+            },
+        getAverageSalesPerEmployee: function () {
 
-        var total=0
-        this.employees.forEach(function(salesTotal){
-            total+= Number(salesTotal.salesUnits)
-        })
-        return total/this.employees.length
-    }
+                var total = 0;
+                this.employees.forEach(function (salesTotal) {
+                    total += Number(salesTotal.salesUnits)
+                });
+                return total / this.employees.length
+            }
+
 
 };
 
-
 console.log(salesReport.getEmployeeCount());
 console.log(salesReport.getTotalNumberOfSales());
-console.log(salesReport.getAverageSalesPerEmployee())
+console.log(salesReport.getAverageSalesPerEmployee());
 
 salesReport.employees.forEach(function(employee){
     console.log(employee.number+ '\n'+ employee.firstName+'\n'+ employee.lastName+'\n'+ employee.salesUnits)
